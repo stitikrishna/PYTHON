2c2ae757d3fd0da5ae3fe5db3bc95b46d127e683
unknown <stitikrishnamohanty@gmail.com>
2023-02-09 15:57:26 +0530

    y first commit

--- SWAYAM PYTHON/02_executing sequence.py
+++
@@ -1,7 +0,0 @@
-a=1
-print(a)
-
-
-a=1;print(a);a=a+1;print(a);a=a+1;print(a);a=a+1;print(a);a=a+1;print(a)
-
-print("hell0 world")
--- SWAYAM PYTHON/03_if condition.py
+++
@@ -1,4 +0,0 @@
-#n=(input("what is the cost?"))
-#print(n)
-#n=15
-#print(n)
